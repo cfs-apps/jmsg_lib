ad85aec8d26d7029ac40dfb84145b128b7d62c84
David McComas <open.stemware@gmail.com>
2026-07-25 09:34:37 -0400

    Close #2

--- fsw/src/lib_cfg.h
+++ fsw/src/lib_cfg.h
@@ -61,9 +61,10 @@
 **
 ** 1.0 - Initial release tested with jmsg_mqtt and jmsg_udp
 ** 1.1 - Updates for JMSG project initial release
+** 3.1 - Compatible with Basecamp 3.x new app version management
 */
 
-#define  JMSG_LIB_MAJOR_VER      1
+#define  JMSG_LIB_MAJOR_VER      3
 #define  JMSG_LIB_MINOR_VER      1
 
 
